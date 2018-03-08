871506c9e68f6197a3f410d76595e3128ea8de61
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-08 10:32:38 -0500

    Added header comment

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 -----------------------------------------------------------------------------------------
---
+-- Title: TouchAndReact
+-- Name: Avery Mack
+-- Course: ICS2O/3C
+-- This program displays a button that when I click on it, turns colour and
+-- displays text and another object.
+-- 
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
