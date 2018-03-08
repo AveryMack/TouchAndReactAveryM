60a4f381e48f6d7570e6047afd6cdca008552775
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-08 11:05:41 -0500

    xchanges

--- main.lua
+++ main.lua
@@ -10,3 +10,25 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- set background colour 
+display.setDefault ("background", 153/255, 204/255, 206/255)
+
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- create blue button, set its position and make it visible 
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true 
+
+-- create red button, set its position and make it invisible
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 96)
+redButton.x = display.newImage 
+
+
+
+
+
+
