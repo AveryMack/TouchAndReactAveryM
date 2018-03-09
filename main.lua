0f942a60618519c8136cf8d3ff1eb40a6e13cc33
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-08 21:55:26 -0500

    changes

--- main.lua
+++ main.lua
@@ -25,7 +25,39 @@ blueButton.isVisible = true
 
 -- create red button, set its position and make it invisible
 local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 96)
-redButton.x = display.newImage 
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false 
+
+-- create text object, set its position make it invisible 
+local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = display.contentHeight/3
+textObject:setTextColor (1, 1, 0)
+textObject.isVisible = false 
+
+-- Function: BlueButtonListener
+-- Input: touch listener
+-- Output: none
+-- Description: when blue button is clicked, it will make
+-- the text apear and make the blue button disapear .
+local function BlueButtonListener(touch) 
+	if (touch.phase == "began") then
+		blueButton.isVisible = false
+		redButton.isVisible = true 
+		textObject.isVisible = true 
+	end 
+
+	if (touch.phase == "ended") then
+	    blueButton.isVisible = true 
+	    redButton.isVisible = false
+	    textObject.isVisible = false 
+	end 
+end 
+
+
+
+
 
 
 
